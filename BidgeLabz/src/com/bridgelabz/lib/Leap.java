da232aab3b1236bbcfce1c95e753bcf31811bde1
Chethu <Chethu@192.168.1.123>
2019-12-19 12:43:31 +0530

    leap

--- BidgeLabz/src/com/bridgelabz/lib/Leap.java
+++ BidgeLabz/src/com/bridgelabz/lib/Leap.java
@@ -1,10 +1,7 @@
 package com.bridgelabz.lib;
-
 import java.util.Scanner;
-
 public class Leap {
 	public static void main(String[] args) {
-
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter the year");
 		int n=sc.nextInt();
@@ -17,29 +14,18 @@ public class Leap {
 			System.out.println("Not leap year");
 		}
 	}	
-		public static boolean leap(int n)
-		{
+	public static boolean leap(int n)
+	{
 		if(n>=1582)
 		{
 			if(n%4==00&&n%100!=0)
-			{
 				return true;
-			}
 			else
 			{
-
-
 				if(n%400==0)
-
 					return true;
-
-			
-
-
-
 			}
 		}
 		return false;
-		}
-	
+	}
 }
